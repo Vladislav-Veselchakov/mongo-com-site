ea47542b35dd219c34952db8b65303d0c71fe26d
Vladislav A <validator1@yandex.ru>
2022-10-12 18:25:22 +0300

    init

---
+++ VL-mongo-baeldung/src/test/java/VLmongobaeldung/VlMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package VLmongobaeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
